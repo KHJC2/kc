6dd0610213831a1054710987eb009b2d108a22e7
1 <1@DESKTOP-SEAM6JJ>
2020-12-23 22:31:44 +0800

    1

--- 壳尘/模块/尘模块.vb
+++ 壳尘/模块/尘模块.vb
@@ -1,19 +1,3 @@
 ﻿Module 尘模块
-    Class 试试
-        Dim AAS
 
-        Sub New(ssdee As Integer)
-            Me.Ssdee = ssdee
-        End Sub
-
-        Property Ssdee As Integer
-
-        Class Ssee
-            Public Function Assee() As Integer
-
-
-
-            End Function
-        End Class
-    End Class
 End Module

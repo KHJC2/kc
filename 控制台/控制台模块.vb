4b01742d2bf6df4eabf83fcd1c4a1f85a60844f5
1 <1@DESKTOP-SEAM6JJ>
2020-12-24 23:29:40 +0800

    2020年12月24日23:29:34

--- 控制台/控制台模块.vb
+++ 控制台/控制台模块.vb
@@ -3,11 +3,11 @@
 Friend Module 控制台模块
     Sub Main()
         System.Console.WriteLine("当地人若")
-        Dim 试试 As Foxtable.DataRow
+        Dim 试试 As Foxtable.DataRow = Nothing
 
-        Dim 傻傻的 = 壳尘.获取对象(试试)
-        Dim 傻傻的1 As 壳尘.人员类型 = CType(傻傻的, 壳尘.人员类型)
-
+        Dim 傻傻的1 As 壳尘.人员类型 = New 壳尘.人员类型(New 壳尘.数据行类型(试试))
+        Console.WriteLine("hello world")
+        Console.ReadLine()
     End Sub
 
 End Module
